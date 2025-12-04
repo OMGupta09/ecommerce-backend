89e39b96a6d0f3ccea78e1175464ad2b98bd7d8a
OM GUPTA <146474653+OMGupta09@users.noreply.github.com>
2025-12-05 01:30:29 +0530

    User auth implemented: JWT based

---
+++ src/main/java/com/om/ecommerce/security/JwtUtil.java
@@ -0,0 +1,56 @@
+package com.om.ecommerce.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key= Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String email,String role){
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role",role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*10))
+                .signWith(key)
+                .compact();
+    }
+
+    //Extract Username
+    public String extractEmail(String token){
+        return extractAllClaims(token).getSubject();
+    }
+
+    //ExtractRole
+    public String extractRole(String token){
+        return (String)extractAllClaims(token).get("role");
+    }
+
+    //Validate Token
+    public boolean isTokenExpired(String token){
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String email){
+        return (extractEmail(token).equals(email) && !isTokenExpired(token));
+    }
+
+    //helper to parse claim itseems
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+}
---
+++ src/main/java/com/om/ecommerce/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.om.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())       // disable csrf
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()   // register + login allowed
+                        .requestMatchers("/products/**").authenticated()  // protect all product APIs
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
